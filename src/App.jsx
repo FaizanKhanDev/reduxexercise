f093370b558df1e49cdd364116bfbff8bcedb14f
FaizanKhanDev <faizan480khan@gmail.com>
2024-01-25 00:53:22 -0800

    created Counter JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import  { useState } from 'react';
+import Counter from './features/counter/Counter';
 
 function App() {
+
   return (
     <div className="App">
-      
+        <Counter/>
     </div>
   );
 }
