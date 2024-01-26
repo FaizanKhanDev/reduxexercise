933d213fd9685e47782c69078061747050a5cf5e
FaizanKhanDev <faizan480khan@gmail.com>
2024-01-25 23:52:08 -0800

    FIXED: redux store

--- src/features/counter/Counter.jsx
+++ src/features/counter/Counter.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
-import  { useState } from 'react';
-
+// import  { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './CounterSlice';
 export default function Counter() {
-  const [ count, setCount ] = useState(0);
+    const count = useSelector((state) =>  state.counter.count);
+    const dispatch = useDispatch();
+//   const [ count, setCount ] = useState(0);
 
     return (
         <div>
             <button className='button'
                 aria-label='Increment value'
-                onClick={() => { setCount(count + 1) }} >
+                onClick={() => {dispatch(increment()) }} >
                 +
             </button>
             <span className='value'>{count}</span>
 
             <button className='button'
                 aria-label='Decrement value'
-                onClick={() => { setCount(count - 1) }}
+                onClick={() => { dispatch(decrement()) }}
             >
                 -
             </button>
